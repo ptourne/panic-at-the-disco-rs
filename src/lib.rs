2dbdf87727de1c9dac75665c1c3797ca83f130ae
Patricio Tourne Passarino <ptourne@fi.uba.ar>
2023-12-27 22:21:40 -0300

    [WIP] Proof of concept in test

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,20 @@
-// this panic macro just calls the panic! macro from the standard library
-#[macro_export]
-macro_rules! panic {
-    ($($x:expr),*) => {{
-        println!("Hey Look Ma, I Made It!");
-        $(
-           // call std panic!
-              std::panic!($x);
-        )*
-    }};
-}
+// // this panic macro just calls the panic! macro from the standard library
+// #[macro_export]
+// macro_rules! panic {
+
+//     ($($x:expr),*) => {{
+//         let mp3_audio = include_bytes!("../assets/hey-look-ma-i-made-it.mp3");
+//         let (_stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
+//         let source = rodio::Decoder::new(mp3_audio).unwrap();
+//         stream_handle.play_raw(source.convert_samples());
+//         std::thread::sleep(std::time::Duration::from_secs(5));
+//         println!("🎩 Hey Look Ma, I Made It! 🕺");
+//         $(
+//            // call std panic!
+//               std::panic!($x);
+//         )*
+//     }};
+// }
 
 #[cfg(test)]
 mod tests {
@@ -16,7 +22,13 @@ mod tests {
 
     #[test]
     fn it_works() {
-        // Check that the panic macro works
-        panic!("This is a test");
+        use rodio::{Decoder, OutputStream, Sink};
+        let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+        let sink = Sink::try_new(&stream_handle).unwrap();
+        let mp3_audio = include_bytes!("../assets/hey-look-ma-i-made-it.mp3");
+        let reader = std::io::Cursor::new(mp3_audio);
+        let source = Decoder::new(reader).unwrap();
+        sink.append(source);
+        sink.sleep_until_end();
     }
 }
