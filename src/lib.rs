f47183e989319c8b0cddae5eda9b273f8f33badf
Patricio Tourne Passarino <ptourne@fi.uba.ar>
2023-12-27 22:29:26 -0300

    [FEAT] Error handling [TEST] Test

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,46 @@
-// // this panic macro just calls the panic! macro from the standard library
-// #[macro_export]
-// macro_rules! panic {
-
-//     ($($x:expr),*) => {{
-//         let mp3_audio = include_bytes!("../assets/hey-look-ma-i-made-it.mp3");
-//         let (_stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
-//         let source = rodio::Decoder::new(mp3_audio).unwrap();
-//         stream_handle.play_raw(source.convert_samples());
-//         std::thread::sleep(std::time::Duration::from_secs(5));
-//         println!("🎩 Hey Look Ma, I Made It! 🕺");
-//         $(
-//            // call std panic!
-//               std::panic!($x);
-//         )*
-//     }};
-// }
+// this panic macro just calls the panic! macro from the standard library
+#[macro_export]
+macro_rules! panic {
+    ($($x:expr),*) => {{
+        use rodio::{Decoder, OutputStream, Sink};
+        let Ok((_stream, stream_handle)) = OutputStream::try_default() else {
+            println!("Failed to create output stream");
+            println!("🎩 Hey Look Ma, I Made It! 🕺");
+            $(
+                  std::panic!($x);
+            )*
+        };
+        let Ok(sink) = Sink::try_new(&stream_handle) else {
+            println!("Failed to create sink");
+            println!("🎩 Hey Look Ma, I Made It! 🕺");
+            $(
+                  std::panic!($x);
+            )*
+        };
+        let mp3_audio = include_bytes!("../assets/hey-look-ma-i-made-it.mp3");
+        let reader = std::io::Cursor::new(mp3_audio);
+        let Ok(source) = Decoder::new(reader) else {
+            println!("Failed to create decoder");
+            println!("🎩 Hey Look Ma, I Made It! 🕺");
+            $(
+                  std::panic!($x);
+            )*
+        };
+        sink.append(source);
+        println!("🎩 Hey Look Ma, I Made It! 🕺");
+        sink.sleep_until_end();
+        $(
+              std::panic!($x);
+        )*
+    }};
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
     #[test]
+    #[should_panic]
     fn it_works() {
-        use rodio::{Decoder, OutputStream, Sink};
-        let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-        let sink = Sink::try_new(&stream_handle).unwrap();
-        let mp3_audio = include_bytes!("../assets/hey-look-ma-i-made-it.mp3");
-        let reader = std::io::Cursor::new(mp3_audio);
-        let source = Decoder::new(reader).unwrap();
-        sink.append(source);
-        sink.sleep_until_end();
+        panic!("This is a test");
     }
 }
